1fcae6bf94864f62b0a7ff8934abad8c4d213b50
Douglas Cunha <dougcunha@gmail.com>
2024-11-20 15:19:38 -0300

    chore: update framework and dependencies
    - Update the target framework to .NET 9.0 for Windows and upgrade DiffPlex to version 1.7.2 and Scintilla5.NET to version 5.6.2.
    - Refactor ScintillaDiffControl to enhance documentation, improve marker management, and expand visual property configurations for more flexible text file comparisons.
    - Convert to a file-scoped namespace, enhance XML documentation, and set designer serialization visibility for the ScrollSync property.
    - Upgrade target framework to .NET 9.0 and add DiffPlex package reference version 1.7.2

--- ScintillaDiff/ScrollSyncScintilla.cs
+++ ScintillaDiff/ScrollSyncScintilla.cs
@@ -30,96 +30,96 @@ using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using ScintillaNET;
 
-namespace ScintillaDiff
+namespace ScintillaDiff;
+
+/// <summary>
+/// An inherited class from the <see cref="Scintilla"/> which can synchronize vertical scroll bar position with another <see cref="Scintilla"/> control.
+/// Implements the <see cref="ScintillaNET.Scintilla" />
+/// </summary>
+/// <seealso cref="ScintillaNET.Scintilla" />
+public class ScrollSyncScintilla : Scintilla
 {
+    // ReSharper disable once CommentTypo
     /// <summary>
-    /// An inherited class from the <see cref="Scintilla"/> which can synchronize vertical scroll bar position with another <see cref="Scintilla"/> control.
-    /// Implements the <see cref="ScintillaNET.Scintilla" />
+    /// The WM_VSCROLL message is sent to a window when a scroll event occurs in the window's standard vertical scroll bar.
+    /// This message is also sent to the owner of a vertical scroll bar control when a scroll event occurs in the control.
+    /// <remarks>
+    /// https://docs.microsoft.com/en-us/windows/desktop/controls/wm-vscroll
+    /// </remarks>
     /// </summary>
-    /// <seealso cref="ScintillaNET.Scintilla" />
-    public class ScrollSyncScintilla : Scintilla
+    // ReSharper disable once InconsistentNaming
+    private const int WM_VSCROLL = 0x115;
+    // ReSharper disable once CommentTypo
+    /// <summary>
+    /// The WM_HSCROLL message is sent to a window when a scroll event occurs in the window's standard horizontal scroll bar.
+    /// This message is also sent to the owner of a horizontal scroll bar control when a scroll event occurs in the control.
+    /// <remarks>
+    /// https://docs.microsoft.com/en-us/windows/desktop/controls/wm-hscroll
+    /// </remarks>
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    private const int WM_HSCROLL = 0x114;
+    /// <summary>
+    /// The WM_MOUSEWHEEL message is Sent to the focus window when the mouse wheel is rotated.
+    /// <remarks>
+    /// https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-mousewheel
+    /// </remarks>
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    private const int WM_MOUSEWHEEL = 0x20a;
+
+    /// <summary>
+    /// The SendMessage Win32 function is used to send a specified message to a window or windows.
+    /// <remarks>
+    /// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessage
+    /// </remarks>
+    /// </summary>
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+    /// <summary>
+    /// Processes Windows messages.
+    /// </summary>
+    /// <param name="m">The Windows Message to process.</param>
+    protected override void WndProc(ref Message m)
     {
-        // ReSharper disable once CommentTypo
-        /// <summary>
-        /// The WM_VSCROLL message is sent to a window when a scroll event occurs in the window's standard vertical scroll bar.
-        /// This message is also sent to the owner of a vertical scroll bar control when a scroll event occurs in the control.
-        /// <remarks>
-        /// https://docs.microsoft.com/en-us/windows/desktop/controls/wm-vscroll
-        /// </remarks>
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        private const int WM_VSCROLL = 0x115;
-        // ReSharper disable once CommentTypo
-        /// <summary>
-        /// The WM_HSCROLL message is sent to a window when a scroll event occurs in the window's standard horizontal scroll bar.
-        /// This message is also sent to the owner of a horizontal scroll bar control when a scroll event occurs in the control.
-        /// <remarks>
-        /// https://docs.microsoft.com/en-us/windows/desktop/controls/wm-hscroll
-        /// </remarks>
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        private const int WM_HSCROLL = 0x114;
-        /// <summary>
-        /// The WM_MOUSEWHEEL message is Sent to the focus window when the mouse wheel is rotated.
-        /// <remarks>
-        /// https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-mousewheel
-        /// </remarks>
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        private const int WM_MOUSEWHEEL = 0x20a;
-
-        /// <summary>
-        /// The SendMessage Win32 function is used to send a specified message to a window or windows.
-        /// <remarks>
-        /// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessage
-        /// </remarks>
-        /// </summary>
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
-
-        /// <summary>
-        /// Processes Windows messages.
-        /// </summary>
-        /// <param name="m">The Windows Message to process.</param>
-        protected override void WndProc(ref Message m)
+        if (m.Msg == WM_MOUSEWHEEL)
         {
-            if (m.Msg == WM_MOUSEWHEEL)
-            {
-                // two casts to make this work with 32 bit and 64 bit params
-                var wp = new IntPtr((int)(long)m.WParam < 0 ? 1 : 0);
-                SendMessage(this.Handle, WM_VSCROLL, wp, new IntPtr(0));
-                return;
-            }
-
-            base.WndProc(ref m);
-
-            // vertical scrolling
-            if (m.Msg == WM_VSCROLL && ScrollSync != null)
-            {
-                // get the first visible line of this control..
-                int scrollTop = FirstVisibleLine;
-
-                // set the value to the other Scintilla control..
-                ScrollSync.FirstVisibleLine = scrollTop;
-            }
-
-            // horizontal scrolling
-            if (m.Msg == WM_HSCROLL && ScrollSync != null)
-            {
-                // get the leftmost pixel
-                int scrollLeft = XOffset;
-
-                // set the value to the other Scintilla control..
-                ScrollSync.XOffset = scrollLeft;
-            }
+            // two casts to make this work with 32 bit and 64 bit params
+            var wp = new IntPtr((int)(long)m.WParam < 0 ? 1 : 0);
+            SendMessage(this.Handle, WM_VSCROLL, wp, new IntPtr(0));
+            return;
         }
 
-        /// <summary>
-        /// Gets or sets the <see cref="Scintilla"/> to synchronize the vertical scroll bar with.
-        /// </summary>
-        [Browsable(true)]
-        [Category("Miscellaneous")]
-        [Description("Gets or sets the Scintilla to synchronize the vertical scroll bar with.")]
-        public Scintilla ScrollSync { get; set; }
+        base.WndProc(ref m);
+
+        // vertical scrolling
+        if (m.Msg == WM_VSCROLL && ScrollSync != null)
+        {
+            // get the first visible line of this control..
+            int scrollTop = FirstVisibleLine;
+
+            // set the value to the other Scintilla control..
+            ScrollSync.FirstVisibleLine = scrollTop;
+        }
+
+        // horizontal scrolling
+        if (m.Msg == WM_HSCROLL && ScrollSync != null)
+        {
+            // get the leftmost pixel
+            int scrollLeft = XOffset;
+
+            // set the value to the other Scintilla control..
+            ScrollSync.XOffset = scrollLeft;
+        }
     }
+
+    /// <summary>
+    /// Gets or sets the <see cref="Scintilla"/> to synchronize the vertical scroll bar with.
+    /// </summary>
+    [Browsable(true)]
+    [Category("Miscellaneous")]
+    [Description("Gets or sets the Scintilla to synchronize the vertical scroll bar with.")]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public Scintilla ScrollSync { get; set; }
 }
